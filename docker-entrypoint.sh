33259f2e1280c262baa06ba8271aef48d1fd5bac
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2016-08-31 11:43:25 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
+/ # cat docker-entrypoint.sh 
 #!/bin/sh
 set -e
 local MYUSER="sickrage"
@@ -8,17 +9,21 @@ local MYUID="10000"
 if [ -n "${DOCKGID}" ]; then
   MYGID="${DOCKGID}"
 fi
-/usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
+if ! /bin/grep -q "${MYUSER}" /etc/group; then
+  /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
+fi
 
 #Managing user
 if [ -n "${DOCKUID}" ]; then
   MYUID="${DOCKUID}"
 fi
-/usr/sbin/adduser -S -D -H -G "${MYUSER}" -u "${MYUID}" "${MYUSER}"
+if ! /bin/grep -q "${MYUSER}" /etc/passwd; then
+  /usr/sbin/adduser -S -D -H -G "${MYUSER}" -u "${MYUID}" "${MYUSER}"
+fi
 
 if [ "$1" = 'sickrage' ]; then
     /bin/chown -R "${MYUSER}" /config /cache
-    exec su-suexec "${MYUSER}" /usr/bin/python /opt/sickrage/SickBeard.py --nolaunch --datadir=/config/ --config=/config/config.ini "$@"
+    exec /sbin/su-exec "${MYUSER}" /usr/bin/python /opt/sickrage/SickBeard.py --nolaunch --datadir=/config/ --config=/config/config.ini "$@"
 fi
 
 exec "$@"
