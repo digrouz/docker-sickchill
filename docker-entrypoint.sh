fb5fa35ee7a833b975fd4b9b041c576b45fbf005
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2016-08-31 10:40:28 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,24 @@
 #!/bin/sh
+set -e
+local MYUSER="sickrage"
+local MYGID="10000"
+local MYUID="10000"
+
+#Managing group
+if [ -n "${DOCKGID}" ]; then
+  MYGID="${DOCKGID}"
+fi
+/usr/sbin/addgroup --system -g "${MYGID}" "${MYUSER}"
+
+#Managing user
+if [ -n "${DOCKUID}" ]; then
+  MYUID="${DOCKUID}"
+fi
+/usr/sbin/adduser -S -D -H -G "${MYUSER}" -u "${MYUID}" "${MYUSER}"
+
+if [ "$1" = 'sickrage' ]; then
+    chown -R "${MYUSER}" /config
+    exec su-suexec /usr/bin/python /opt/sickrage/SickBeard.py --datadir=/config/ --config=/config/config.ini "$@"
+fi
+
+exec "$@"
