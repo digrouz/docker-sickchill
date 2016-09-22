5376d4e19f30245eb80af827ddc43d72c93652d1
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2016-09-22 15:11:11 +0200

    improved user management

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,21 +4,47 @@ local MYUSER="sickrage"
 local MYGID="10000"
 local MYUID="10000"
 
-#Managing group
-if [ -n "${DOCKGID}" ]; then
-  MYGID="${DOCKGID}"
-fi
-if ! /bin/grep -q "${MYUSER}" /etc/group; then
+ConfigureUser () {
+  # Managing user
+  if [ -n "${DOCKUID}" ]; then
+    MYUID="${DOCKUID}"
+  fi
+  # Managing group
+  if [ -n "${DOCKGID}" ]; then
+    MYGID="${DOCKGID}"
+  fi
+  local OLDHOME
+  local OLDGID
+  local OLDUID
+  local USEREXISTS=$(/bin/grep -q "${MYUSER}" /etc/passwd)
+  if [ $USEREXISTS -eq 0 ]; then
+    OLDUID=$(/usr/bin/id -u "${MYUSER}")
+    OLDGID=$(/usr/bin/id -g "${MYUSER}")
+    if [ "${DOCKUID}" != "${OLDUID}" ]; then
+      OLDHOME=$(/bin/echo "~${MYUSER}")
+      /usr/sbin/deluser "${MYUSER}"
+      /usr/bin/logger "Deleted user ${MYUSER}"
+    fi
+    local GROUPEXISTS=$(/bin/grep -q "${MYUSER}" /etc/group)
+    if [ $GROUPEXISTS -eq 0 ]; then
+      local OLDGID=$(/usr/bin/id -g "${MYUSER}")
+      if [ "${DOCKGID}" != "${OLDGID}" ]; then
+        /usr/sbin/delgroup "${MYUSER}"
+        /usr/bin/logger "Deleted group ${MYUSER}"
+      fi
+    fi
+  fi
   /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
-fi
+  /usr/sbin/adduser -S -D -H -s /sbin/nologin -G "${MYUSER}" -h "${OLDHOME}" -u "${MYUID}" "${MYUSER}"
+  if [ -n "${OLDUID}" ] && [ "${DOCKUID}" != "${OLDUID}" ]; then
+    /usr/bin/find / -user "${OLDUID}" -exec /bin/chown ${MYUSER} {} \;
+  fi
+  if [ -n "${OLDGID}" ] && [ "${DOCKGID}" != "${OLDGID}" ]; then
+    /usr/bin/find / -group "${OLDGID}" -exec /bin/chgrp ${MYUSER} {} \;
+  fi
+}
 
-#Managing user
-if [ -n "${DOCKUID}" ]; then
-  MYUID="${DOCKUID}"
-fi
-if ! /bin/grep -q "${MYUSER}" /etc/passwd; then
-  /usr/sbin/adduser -S -D -H -G "${MYUSER}" -u "${MYUID}" "${MYUSER}"
-fi
+ConfigureUser
 
 if [ "$1" = 'sickrage' ]; then
     /bin/chown -R "${MYUSER}":"${MYUSER}" /config /opt/sickrage
