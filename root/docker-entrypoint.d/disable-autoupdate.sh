d4b5451f9f06e48cdd54dbd9708a7197857efcd3
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2020-09-15 22:12:35 +0200

    improve script

--- root/docker-entrypoint.d/disable-autoupdate.sh
+++ root/docker-entrypoint.d/disable-autoupdate.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+
+. /etc/profile
+. /usr/local/bin/docker-entrypoint-functions.sh
+
+MYUSER="${APPUSER}"
+MYUID="${APPUID}"
+MYGID="${APPGID}"
+
+DockLog "Disabling auto_update in /config/config.ini"
 sed -i \
   -e 's|auto_update =.*|auto_update = 0|g' \
 /config/config.ini
