ea205fd8bb7e70058b67ce767e834970be78b269
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2021-04-15 15:55:13 +0200

    added github actions

---
+++ .github/scripts/update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+SICKCHILL_URL="https://api.github.com/repos/sickchill/sickchill/tags"
+
+
+FULL_LAST_VERSION=$(curl -SsL ${SICKCHILL_URL} | jq .[0].name -r )
+LAST_VERSION="${FULL_LAST_VERSION:1}"
+
+sed -i -e "s|SICKCHILL_VERSION='.*'|SICKCHILL_VERSION='${LAST_VERSION}'|" Dockerfile_*
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+  echo "No new version available!"
+else 
+  # Uncommitted changes
+  git commit -a -m "update to version: ${LAST_VERSION}"
+  git push
+fi
