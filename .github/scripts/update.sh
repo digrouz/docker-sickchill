94e397fd769e03818da026fd3ec30271db83b6fd
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2021-04-19 13:36:17 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -6,7 +6,7 @@ SICKCHILL_URL="https://api.github.com/repos/sickchill/sickchill/tags"
 FULL_LAST_VERSION=$(curl -SsL ${SICKCHILL_URL} | jq .[0].name -r )
 LAST_VERSION="${FULL_LAST_VERSION:1}"
 
-sed -i -e "s|SICKCHILL_VERSION='.*'|SICKCHILL_VERSION='${LAST_VERSION}'|" Dockerfile_*
+sed -i -e "s|SICKCHILL_VERSION='.*'|SICKCHILL_VERSION='${LAST_VERSION}'|" Dockerfile*
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
