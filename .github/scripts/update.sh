af4ee1f5ab6d9ff0292c4771878e3a4526c11d63
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2021-08-17 08:55:07 +0000

    update to version: 021.07.29

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-SICKCHILL_URL="https://api.github.com/repos/sickchill/sickchill/tags"
+SICKCHILL_URL="https://api.github.com/repos/sickchill/sickchill/git/refs/tags"
 
-
-FULL_LAST_VERSION=$(curl -SsL ${SICKCHILL_URL} | jq .[0].name -r )
+FULL_LAST_VERSION=$(curl -SsL ${SICKCHILL_URL} | jq '.[-1].ref' -r | sed -e 's|refs/tags/||')
 LAST_VERSION="${FULL_LAST_VERSION:1}"
 
 sed -i -e "s|SICKCHILL_VERSION='.*'|SICKCHILL_VERSION='${LAST_VERSION}'|" Dockerfile*
